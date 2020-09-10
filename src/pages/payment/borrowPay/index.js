c36f4f8a98bdba03339ccc7f40447e05a8d7cb55
tanwei <tanwei@jimistore.com>
2020-09-10 10:53:17 +0800

    需求:付款

---
+++ src/pages/payment/borrowPay/index.js
@@ -0,0 +1,434 @@
+import React from 'react';
+import { message, Form, Modal } from 'antd';
+import moment from 'moment';
+import { connect } from 'dva';
+// import { formItemLayout } from '@/utils/constants';
+// import InvoiceDetail from '@/components/Modals/InvoiceDetail';
+// import Search from 'antd/lib/input/Search';
+import { rowSelect } from '@/utils/common';
+import constants from '@/utils/constants';
+import PayTemp from './components/PayTemp';
+
+const { confirm } = Modal;
+const { APP_API } = constants;
+@Form.create()
+@connect(({ loading, borrowPay }) => ({
+  loading: loading.effects['borrowPay/list'] || false,
+  list: borrowPay.list,
+  query: borrowPay.query,
+  total: borrowPay.total,
+}))
+class BorrowPay extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: '2',
+      selectedRowKeys: [],
+      count: 0,
+      sumAmount: 0,
+      searchContent: '',
+      selectedRows: [],
+    };
+  }
+
+  componentDidMount(){
+    const {
+      query,
+    } = this.props;
+    this.onQuery({
+      ...query,
+      status: 2,
+    });
+  }
+
+  handleClick = e => {
+    const { query } = this.props;
+    const createTime = this.props.form.getFieldValue('createTime');
+    let startTime = '';
+    let endTime = '';
+    if (createTime && createTime.length > 0) {
+      startTime = moment(createTime[0]).format('x');
+      endTime = moment(createTime[1]).format('x');
+    }
+    const { searchContent } = this.state;
+    this.setState({
+      status: e.key,
+      selectedRowKeys: [],
+      selectedRows: [],
+      sumAmount: 0,
+    });
+    this.onQuery({
+      ...query,
+      status: e.key,
+      searchContent,
+      startTime,
+      endTime,
+      pageNo: 1,
+    });
+  };
+
+  onSelectAll = (selected, selectedRows, changeRows) => {
+    const result = rowSelect.onSelectAll(this.state, selected, changeRows);
+    const _selectedRows = result.selectedRows;
+    const { selectedRowKeys } = result;
+    let amount = 0;
+    _selectedRows.forEach(item => {
+      amount+=item.submitSum;
+    });
+
+    this.setState({
+        selectedRows: _selectedRows,
+        selectedRowKeys,
+        sumAmount: amount,
+    });
+  };
+
+  onSelect = (record, selected) => {
+      const {
+          selectedRows,
+          selectedRowKeys,
+      } = rowSelect.onSelect(this.state, record, selected);
+      console.log(selectedRowKeys);
+      let amount = 0;
+      selectedRows.forEach(item => {
+        amount+=item.submitSum;
+      });
+      this.setState({
+          selectedRows,
+          selectedRowKeys,
+          sumAmount: amount,
+      });
+  };
+
+  onDelete = (id) => {
+      const {
+          selectedRows,
+          selectedRowKeys,
+      } = rowSelect.onDelete(this.state, id);
+      let amount = 0;
+      selectedRows.forEach(item => {
+        amount+=item.submitSum;
+      });
+      this.setState({
+          selectedRows,
+          selectedRowKeys,
+          sumAmount: amount,
+      });
+  };
+
+  onOk = () => {
+    const {
+      query,
+    } = this.props;
+    const createTime = this.props.form.getFieldValue('createTime');
+    let startTime = '';
+    let endTime = '';
+    if (createTime && createTime.length > 0) {
+      startTime = moment(createTime[0]).format('x');
+      endTime = moment(createTime[1]).format('x');
+    }
+    const { status, searchContent } = this.state;
+    this.onQuery({
+      ...query,
+      status,
+      startTime,
+      endTime,
+      searchContent,
+    });
+  }
+
+  handChange = (date) => {
+    if (!date) {
+      const { status, searchContent } = this.state;
+      const {
+        query,
+      } = this.props;
+      this.onQuery({
+        ...query,
+        status,
+        searchContent,
+      });
+    }
+  }
+
+  onLink = (id) => {
+    this.props.history.push(`/system/auth/${id}`);
+  }
+
+  onQuery = (payload) => {
+    this.props.dispatch({
+      type: 'borrowPay/list',
+      payload,
+    });
+  }
+
+  onChange = (rows, keys) => {
+    let amount = 0;
+    keys.forEach(item => {
+      if (item.submitSum) {
+        amount+=item.submitSum;
+      }
+    });
+    this.setState({
+      selectKey: keys,
+      count: keys.length,
+      sumAmount: amount/100,
+    });
+  }
+
+  onSearch = (val) => {
+    const { query } = this.props;
+    const { status } = this.state;
+    const createTime = this.props.form.getFieldValue('createTime');
+    let startTime = '';
+    let endTime = '';
+    if (createTime && createTime.length > 0) {
+      startTime = moment(createTime[0]).format('x');
+      endTime = moment(createTime[1]).format('x');
+    }
+    this.setState({
+      searchContent: val,
+    });
+    this.onQuery({
+      ...query,
+      searchContent: val,
+      status,
+      startTime,
+      endTime,
+    });
+  }
+
+  export = (key) => {
+    const { selectedRowKeys, status, searchContent } = this.state;
+    if (selectedRowKeys.length ===  0 && key === '1') {
+      message.error('请选择要导出的数据');
+      return;
+    }
+    let params = {};
+    const createTime = this.props.form.getFieldValue('createTime');
+    let startTime = '';
+    let endTime = '';
+    if (createTime && createTime.length > 0) {
+      startTime = moment(createTime[0]).format('x');
+      endTime = moment(createTime[1]).format('x');
+    }
+    let url = 'borrowPay/exporting';
+    if (key === '1') {
+      params = {
+        ids: selectedRowKeys
+      };
+    } else if (key === '2') {
+      params = {
+        searchContent,
+        startTime,
+        endTime,
+      };
+    }
+    if(Number(status) !== 2) {
+      url = 'borrowPay/exported';
+    }
+    this.props.dispatch({
+      type: url,
+      payload: {
+        ...params,
+      }
+    }).then(() => {
+      message.success('导出成功');
+    });
+  }
+
+  print = () => {
+    const { selectedRowKeys } = this.state;
+    if (selectedRowKeys.length > 1) {
+      message.error('只支持打印一条数据');
+      return;
+    }
+    if (selectedRowKeys.length === 0) {
+      message.error('请选择一条数据打印');
+      return;
+    }
+    window.location.href = `${APP_API}/cost/export/pdfDetail?token=${localStorage.getItem('token')}&id=${selectedRowKeys[0]}`;
+    // this.props.dispatch({
+    //   type: 'global/print',
+    //   payload: {
+    //     id: selectedRowKeys[0],
+    //   }
+    // }).then(() => {
+    //   message.success('打印成功');
+    // });
+  }
+
+  // 拒绝
+  handleRefuse = (id, callback) => {
+    confirm({
+      title: '确认拒绝该单据？',
+      onOk: () => {
+        this.props.dispatch({
+          type: 'borrowPay/refuse',
+          payload: {
+            invoiceSubmitIds: [id]
+          }
+        }).then(() => {
+          callback();
+          this.onOk();
+        });
+      }
+    });
+  }
+
+  render() {
+    const {
+      list,
+      query,
+      total,
+      loading,
+    } = this.props;
+    // const columns = [{
+    //   title: '报销事由',
+    //   dataIndex: 'reason',
+    //   width: 150,
+    // }, {
+    //   title: '金额(元)',
+    //   dataIndex: 'submitSum',
+    //   render: (text) => (
+    //     <span>{text/100}</span>
+    //   ),
+    //   className: 'moneyCol',
+    //   width: 100,
+    // }, {
+    //   title: '单号',
+    //   dataIndex: 'invoiceNo',
+    //   width: 130,
+    // }, {
+    //   title: '单据类型',
+    //   dataIndex: 'invoiceTemplateName',
+    //   width: 120,
+    //   render: (text) => (
+    //     <span>{text || '-'}</span>
+    //   )
+    // }, {
+    //   title: '收款账户名称',
+    //   dataIndex: 'receiptName',
+    //   width: 120,
+    //   render: (_, record) => {
+    //     let name = record.receiptName;
+    //     if (record.supplierAccountVo && record.supplierAccountVo.supplierAccountName) {
+    //       name = record.supplierAccountVo.supplierAccountName;
+    //     }
+    //     return (
+    //       <span>{name || '-'}</span>
+    //     );
+    //   }
+    // }, {
+    //   title: '个人/供应商收款账户',
+    //   dataIndex: 'receiptNameJson',
+    //   render: (_, record) => {
+    //     let account = record.receiptNameJson && JsonParse(record.receiptNameJson);
+    //     if (record.supplierAccountVo && record.supplierAccountVo.supplierAccountName) {
+    //       account = [{
+    //         ...record.supplierAccountVo,
+    //         type: record.supplierAccountVo.accountType,
+    //         account: record.supplierAccountVo.supplierAccount,
+    //       }];
+    //     }
+    //     return (
+    //       <span>
+    //         {account && account[0] && account[0].type ? getArrayValue(account[0].type, accountType) : ''}
+    //         { account && account[0] && account[0].bankName }
+    //         { account && account[0] && account[0].account }
+    //         {!account && '-'}
+    //       </span>
+    //     );
+    //   },
+    //   width: 140,
+    // }, {
+    //   title: '提交人',
+    //   dataIndex: 'createName',
+    //   width: 100,
+    // }, {
+    //   title: '提交时间',
+    //   dataIndex: 'createTime',
+    //   render: (text) => (
+    //     <span>{ text && moment(text).format('YYYY-MM-DD') }</span>
+    //   ),
+    //   width: 100,
+    // }, {
+    //   title: '操作',
+    //   dataIndex: 'ope',
+    //   render: (_, record) => (
+    //     <span>
+    //       {
+    //         Number(record.status) === 2 &&
+    //           <PayModal onOk={() => this.onOk()} data={record}>
+    //             <a>标记已付</a>
+    //           </PayModal>
+    //       }
+    //       {
+    //         Number(record.status) === 2 &&
+    //         <Divider type="vertical" />
+    //       }
+    //       <InvoiceDetail id={record.invoiceId} canRefuse={Number(record.status) === 2} refuse={this.handleRefuse}>
+    //         <a>查看</a>
+    //       </InvoiceDetail>
+    //     </span>
+    //   ),
+    //   width: 140,
+    //   fixed: 'right',
+    //   className: 'fixCenter'
+    // }];
+    // if(Number(status) !== 2) {
+    //   columns.splice(8, 0, {
+    //     title: '发放人',
+    //     dataIndex: 'payUserName',
+    //     width: 100,
+    //   }, {
+    //     title: '付款时间',
+    //     dataIndex: 'payTime',
+    //     render: (text) => (
+    //       <span>{ text && moment(text).format('YYYY-MM-DD') }</span>
+    //     ),
+    //     width: 100,
+    //   }, {
+    //     title: '付款账户',
+    //     dataIndex: 'payNameJson',
+    //     render: (_, record) => {
+    //       const account = record.payNameJson && JsonParse(record.payNameJson);
+    //       return (
+    //         <span>
+    //           {account && account[0] && account[0].type ? getArrayValue(account[0].type, accountType) : ''}
+    //           <span className="m-r-8">{ account && account[0] && account[0].bankName }</span>
+    //           { account && account[0] && account[0].account }
+    //         </span>
+    //       );
+    //     },
+    //     width: 140,
+    //   }, {
+    //     title: '付款账户名称',
+    //     dataIndex: 'payName',
+    //     render: (_, record) => {
+    //       const account = record.payNameJson && JsonParse(record.payNameJson);
+    //       return (
+    //         <span>
+    //           { account && account[0] && account[0].name }
+    //         </span>
+    //       );
+    //     },
+    //     width: 140,
+    //   });
+    // };
+    return (
+      <>
+        <PayTemp
+          list={list}
+          query={query}
+          total={total}
+          loading={loading}
+          {...this.props}
+          templateType={0}
+        />
+      </>
+    );
+  }
+}
+
+export default BorrowPay;
