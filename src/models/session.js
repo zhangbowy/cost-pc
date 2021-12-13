83d9038049bbfdc0a1f52ed459738a084997ab5a
tanwei <2534234979@qq.com>
2021-12-13 14:05:04 +0800

    fix: 分公司

--- src/models/session.js
+++ src/models/session.js
@@ -79,8 +79,8 @@ export default {
     *login({ payload }, { call, put }) {
       const response = yield call(post, api.login, payload);
       // Object.assign(payload, {
-      //   corpId: 'dinga1f85979a9ebc663',
-      //   dingUserId: '395950298074'
+      //   corpId: 'ding6aebe0e39e99e30b35c2f4657eb6378f',
+      //   dingUserId: '0229355333508937'
       // });
       // const response = yield call(get, api.mockLogin, payload);
       Session.set('userInfo', JSON.stringify(response));
