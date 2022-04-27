eb007d9e2d0914d4043d16af0076e390b77d0289
‘liuxiangming’ <15708072132@qq.com>
2022-04-27 09:40:51 +0800

    字段联动部分

--- src/pages/basicSettings/index.js
+++ src/pages/basicSettings/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable react/jsx-closing-tag-location */
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import withRouter from 'umi/withRouter';
 import style from './index.scss';
+import InitModal from '@/components/Modals/InitModal';
 
 @withRouter
 @connect(({ session, loading }) => ({
@@ -61,28 +63,53 @@ class basicSetting extends Component {
 		});
 	}
 
-	loadmenu = (data) => {
-		const that = this;
-		return data.map(item => (
-  <div
-    className={style.card}
-    onClick={() => that.goMenuUrl(item)}
-    key={item.id}
-  >
-    <div className={style.card_left}>
-      {
+	isCanUse = (item,qrCode) => {
+	const that = this;
+	return   qrCode?<div
+  className={style.card}
+  key={item.id}
+ >
+  <div className={style.card_left}>
+    {
         item.icon &&
         <div className={`${style.iconImg} ${style[item.icon]}`}  />
       }
-    </div>
-    <div className={style.card_right} style={item.note?{}:{justifyContent:'center'}} >
-      <div className={style.card_right_title}>{item.menuName}</div>
-      {
+  </div>
+  <div className={style.card_right} style={item.note?{}:{justifyContent:'center'}} >
+    <div className={style.card_right_title}>{item.menuName}</div>
+    {
         item.note &&
         <div className={style.card_right_content}>{item.note}</div>
       }
-    </div>
   </div>
+</div>:<div
+  className={style.card}
+  onClick={() => that.goMenuUrl(item)}
+  key={item.id}
+>
+  <div className={style.card_left}>
+    {
+        item.icon &&
+        <div className={`${style.iconImg} ${style[item.icon]}`}  />
+      }
+  </div>
+  <div className={style.card_right} style={item.note?{}:{justifyContent:'center'}} >
+    <div className={style.card_right_title}>{item.menuName}</div>
+    {
+        item.note &&
+        <div className={style.card_right_content}>{item.note}</div>
+      }
+  </div>
+</div>;
+	}
+	
+	loadmenu = (data) => {
+	return data.map(item => (
+//  sort不等于25,26,27时正常使用，然后判断sort 25,26,27的情况
+// eslint-disable-next-line no-nested-ternary
+		(item.sort !== 25 && item.sort !== 26 && item.sort !== 27) ? this.isCanUse(item, false) : item.sort === 25 && item.url!=='**QRcode' ?
+			this.isCanUse(item, false) : (item.sort !== 26 || item.sort !== 27) && item.url!=='**QRcode' ?
+				this.isCanUse(item, false): <InitModal isOpen='true' key={item.id}>{this.isCanUse(item, true)}</InitModal>
     ));
 	}
 
